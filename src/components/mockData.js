251284f69db1b2dc71191144a19422821f8a7b0a
bdthinh <bdthinh@fit.hcmus.edu.vn>
2017-10-23 15:02:51 +0700

    Chore: add mockData

---
+++ src/components/mockData.js
@@ -0,0 +1,30 @@
+export default {
+  restaurants: [
+    {
+      name: 'Bun bo Hue Hung Map',
+      dishes: [
+        'Bun bo Hue',
+        'Bun bo bap hoa',
+        'Bun them',
+        'Bun moc',
+      ],
+    },
+    {
+      name: 'Com tam thuan kieu',
+      dishes: [
+        'Com suon',
+        'Com suon bi',
+        'Com muc don thit',
+        'Canh kho qua',
+        'Canh rau ngot',
+      ],
+    },
+    {
+      name: 'Salad',
+      dishes: [
+        'Salad trang',
+        'Salad xanh',
+      ],
+    },
+  ],
+};
