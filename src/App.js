5fcacd4111e9efc1978ad4fa2379213b6fa8c3fc
bdthinh <bdthinh@fit.hcmus.edu.vn>
2017-10-24 23:57:38 +0700

    Feat: add Provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import { Provider } from 'react-redux';
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,19 +10,21 @@ import Layout from './Layout';
 
 const today = new Date().toISOString().slice(0, 10);
 
-const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Lunch order with React</h1>
-      </header>
-      <p className="App-intro">
-        Today is {today}
-      </p>
-      <Layout />
-    </div>
-  </MuiThemeProvider>
+const App = ({ store }) => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Lunch order with React</h1>
+        </header>
+        <p className="App-intro">
+          Today is {today}
+        </p>
+        <Layout />
+      </div>
+    </MuiThemeProvider>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import createStore from './state/createStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    // eslint-disable-next-line
+    console.log('App hot reloaded');
+    // eslint-disable-next-line global-require
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp store={store} history={history} />,
+      document.getElementById('root')
+    );
+  });
+}
+
 registerServiceWorker();
